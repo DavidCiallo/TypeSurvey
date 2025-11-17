8419f344d288a658de603d565952dafc35ad7bbd
DcolorWei <corfer@yeah.net>
2025-11-17 14:52:22 +0800

    auth action before login

--- server/lib/crypto.ts
+++ server/lib/crypto.ts
@@ -1,27 +1,39 @@
-import crypto from 'crypto';
+import crypto from "crypto";
 
-const secretKey = crypto.createHash('sha256').update("MySecretPassphrase1234567890abcdefghijklmnopqrstuv").digest();
+const secret = process.env.SECRET;
+const noncelen = Number(process.env.NONCE_LENGTH);
+
+if (!secret || isNaN(noncelen)) {
+    throw new Error("Missing or invalid SECRET or NONCE_LENGTH in environment variables");
+}
+
+const secretKey = crypto.createHash("sha256").update(secret).digest();
 const iv = crypto.randomBytes(16);
 
-export function aesEncrypt(originalData: string) {
-    const dataToEncrypt = originalData;
-    const cipher = crypto.createCipheriv('aes-256-cbc', secretKey, iv);
-    let encryptedData = cipher.update(dataToEncrypt, 'utf8', 'hex');
-    encryptedData += cipher.final('hex');
+export function aesEncrypt(originalData: string): string {
+    const nonce = crypto.randomBytes(128).toString("hex").slice(-noncelen);
+    const dataToEncrypt = nonce + originalData.split("").reverse().join("");
+    const cipher = crypto.createCipheriv("aes-256-cbc", secretKey, iv);
+    let encryptedData = cipher.update(dataToEncrypt, "utf8", "hex");
+    encryptedData += cipher.final("hex");
     return encryptedData;
 }
 
-export function aesDecrypt(encryptedData: string) {
+export function aesDecrypt(encryptedData: string): string | null {
     try {
-        const decipher = crypto.createDecipheriv('aes-256-cbc', secretKey, iv);
-        let decryptedData = decipher.update(encryptedData, 'hex', 'utf8');
-        decryptedData += decipher.final('utf8');
-        return decryptedData;
+        const decipher = crypto.createDecipheriv("aes-256-cbc", secretKey, iv);
+        let decryptedData = decipher.update(encryptedData, "hex", "utf8");
+        decryptedData += decipher.final("utf8");
+        return decryptedData.slice(noncelen).split("").reverse().join("");
     } catch {
         return null;
     }
 }
 
-export function hashGenerate(originalData: string) {
-    return crypto.createHash('sha256').update(originalData).digest('hex');
+export function hashGenerate(originalData: string): string {
+    return crypto.createHash("sha256").update(originalData).digest("hex");
+}
+
+export function codeGenerate(originalData: string): string {
+    return String(1000 + (([...originalData].reduce((a, c) => a + c.charCodeAt(0), 1) * noncelen) % 9000));
 }
