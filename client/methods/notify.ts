37732dda0dc7427a0a09a6d6f1752f49630c065e
DcolorWei <corfer@yeah.net>
2025-11-25 00:24:00 +0800

    refactor formlist page

--- client/methods/notify.ts
+++ client/methods/notify.ts
@@ -6,21 +6,21 @@ export function notify() {
 
     const notes = [
         { frequency: 20000, duration: 0.3 },
-        { frequency: 783.99, duration: 0.25 },   // G5
-        { frequency: 659.25, duration: 0.25 },   // E5
-        { frequency: 880.00, duration: 0.25 },   // A5
-        { frequency: 698.46, duration: 0.25 },  // F5
-        { frequency: 1046.5, duration: 0.5 }    // C6
+        { frequency: 783.99, duration: 0.25 }, // G5
+        { frequency: 659.25, duration: 0.25 }, // E5
+        { frequency: 880.0, duration: 0.25 }, // A5
+        { frequency: 698.46, duration: 0.25 }, // F5
+        { frequency: 1046.5, duration: 0.5 }, // C6
     ];
 
     let currentTime = audioCtx.currentTime;
 
-    notes.forEach(note => {
+    notes.forEach((note) => {
         const oscillator = audioCtx.createOscillator();
         const gainNode = audioCtx.createGain();
-        oscillator.type = 'sine';
+        oscillator.type = "sine";
         oscillator.frequency.value = note.frequency;
-        const [attack, release, volume] = [0.05, 0.2, 0.1]
+        const [attack, release, volume] = [0.05, 0.2, 0.1];
         gainNode.gain.setValueAtTime(0, currentTime);
         gainNode.gain.linearRampToValueAtTime(volume, currentTime + attack);
         gainNode.gain.setValueAtTime(volume, currentTime + note.duration - release);
@@ -38,18 +38,5 @@ export function notify() {
 
 export function toast({ ...props }: Partial<ToastProps>) {
     closeAll();
-    if (!props.title) return;
-    const asciiArray = Array.from(String(props.title)).map(char => char.charCodeAt(0));
-    const decode = asciiArray.reduce((acc, cur) => acc * 10 + cur, 0).toString(36).slice(0, 8);
-    const list: Array<string> = JSON.parse(localStorage.getItem("toast") || "[]");
-    if (!list.includes(decode)) {
-        list.push(decode);
-        localStorage.setItem("toast", JSON.stringify(list));
-        setTimeout(() => {
-            const nlist = list.filter(item => item !== decode);
-            localStorage.setItem("toast", JSON.stringify(nlist));
-        }, 1500);
-        props.timeout = 2500;
-        addToast(props);
-    }
+    addToast(props);
 }
---
+++ client/pages/form/FormAddBtn.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@heroui/react";
+import { AcceptType, FileUpload } from "../../components/control/FileUpload";
+import { Locale } from "../../methods/locale";
+
+type props = {
+    openFormEditor: () => void;
+    uploadXlsx: (file: File | null) => void;
+};
+
+const FormAddBtn = ({ openFormEditor, uploadXlsx }: props) => {
+    const locale = Locale("FormPage");
+
+    return (
+        <div className="flex flex-row">
+            <Button onClick={() => openFormEditor()} color="default" variant="bordered" className="text-black-500">
+                {locale.CreateNewForm}
+            </Button>
+            <FileUpload
+                element={
+                    <Button color="default" variant="bordered" className="text-black-500">
+                        {locale.ImportXlsx}
+                    </Button>
+                }
+                accept={[AcceptType.CSV, AcceptType.XLS, AcceptType.XLSX]}
+                upload={uploadXlsx}
+            />
+        </div>
+    );
+};
+
+export default FormAddBtn;
