c231b57c4573f41ec0be037c8e252441091dc687
DcolorWei <corfer@yeah.net>
2025-11-28 06:53:48 +0800

    add copy link and content buttons in feedback page

--- client/pages/record/RecordPage.tsx
+++ client/pages/record/RecordPage.tsx
@@ -17,11 +17,14 @@ import { FormFieldRouter, RecordRouter } from "../../api/instance";
 import { toast } from "../../methods/notify";
 import { FormFieldImpl, RecordImpl } from "../../../shared/impl";
 import { Locale } from "../../methods/locale";
+import { copytext } from "../../methods/text";
 
 const Component = () => {
     const locale = Locale("RecordPage");
 
-    const [recordList, setRecordList] = useState<Array<{ item_id: string; data: RecordImpl[] }>>([]);
+    const baseurl = location.protocol + "//" + location.host + "/fill?t=";
+
+    const [recordList, setRecordList] = useState<Array<{ item_id: string; code: string; data: RecordImpl[] }>>([]);
     const [fieldList, setFieldList] = useState<Array<FormFieldImpl>>([]);
     const [fieldChoose, setFieldChoose] = useState<FormFieldImpl | null>(null);
     const [itemChoose, setItemChoose] = useState<string | null>(null);
@@ -29,11 +32,9 @@ const Component = () => {
     const [userpage, setPage] = useState(1);
     const [usertotal, setTotal] = useState(1);
 
-    const [fieldpage, setFieldPage] = useState(1);
-    const [fieldtotal, setFieldTotal] = useState(1);
+    const [_, setFieldTotal] = useState(1);
 
     async function loadUserPage(page: number) {
-        setPage(page);
         setItemChoose(null);
         const form_name = localStorage.getItem("formname") || "";
         const { data } = await RecordRouter.all({ form_name, page });
@@ -42,9 +43,9 @@ const Component = () => {
         }
         setTotal(Math.ceil(data.total / 10) || 1);
         setRecordList(data.records);
+        setPage(page);
     }
-    async function loadFieldPage(page: number) {
-        setFieldPage(page);
+    async function loadFieldPage(page: number = 1) {
         const form_name = localStorage.getItem("formname") || "";
 
         const { success, data, message } = await FormFieldRouter.list({ form_name, page });
@@ -60,12 +61,17 @@ const Component = () => {
         setFieldList([...fieldList]);
         setFieldTotal(Math.ceil(total / 10));
         if (!fieldChoose || !fieldList.some((f) => f.id === fieldChoose.id)) {
-            setFieldChoose(list[0] || null);
+            const usefulField = fieldList.find((i) => !i.disabled);
+            setFieldChoose(usefulField || null);
+        }
+        if (list.length > 0 && fieldList.length < total) {
+            loadFieldPage(page + 1);
+        } else {
         }
     }
     useEffect(() => {
         loadUserPage(userpage);
-        loadFieldPage(fieldpage);
+        loadFieldPage(1);
     }, []);
 
     return (
@@ -75,7 +81,7 @@ const Component = () => {
                 <div className="flex flex-row justify-between items-center w-full py-2">
                     <Select
                         aria-label="select"
-                        className="w-36"
+                        className="w-1/6"
                         variant="bordered"
                         selectedKeys={[fieldChoose?.id || ""]}
                         onSelectionChange={(key) => setFieldChoose(fieldList.find((f) => f.id === key.currentKey)!)}
@@ -94,8 +100,9 @@ const Component = () => {
                     </Button>
                 </div>
                 <div className="mt-2 flex flex-row justify-between items-start gap-4">
-                    <Card className="min-w-[450px] w-1/3 h-[70vh]">
+                    <Card className="min-w-[450px] min-w-1/3 max-w-1/3 h-[70vh]">
                         <Table
+                            isStriped
                             aria-label="table"
                             bottomContent={
                                 <div className="flex items-center">
@@ -126,15 +133,25 @@ const Component = () => {
                                             <TableCell align="center" className="min-w-28 max-w-28">
                                                 {time.toLocaleString().slice(5, 16)}
                                             </TableCell>
-                                            <TableCell align="center" className="flex flex-row justify-center gap-2">
+                                            <TableCell align="center" className="flex flex-row justify-center gap-1">
                                                 <Button
                                                     variant="bordered"
                                                     size="sm"
-                                                    className="mx-4"
                                                     onClick={() => setItemChoose(i.item_id)}
                                                     color={i.item_id === itemChoose ? "primary" : "default"}
                                                 >
                                                     {locale.ListViewRecordButton}
+                                                </Button>
+                                                <Button
+                                                    variant="bordered"
+                                                    size="sm"
+                                                    onClick={() => {
+                                                        copytext(`${baseurl + i.item_id}#code:${i.code}`);
+                                                        toast({ title: locale.ToastCopySuccess, color: "success" });
+                                                    }}
+                                                    color="danger"
+                                                >
+                                                    {locale.ListItemLinkButton}
                                                 </Button>
                                             </TableCell>
                                         </TableRow>
@@ -145,27 +162,14 @@ const Component = () => {
                     </Card>
 
                     <Card className="w-2/3 h-[70vh]">
-                        <Table
-                            aria-label="table"
-                            className="h-full"
-                            bottomContent={
-                                <div className="flex items-center">
-                                    <Pagination
-                                        className="mx-auto"
-                                        initialPage={1}
-                                        total={fieldtotal}
-                                        onChange={loadFieldPage}
-                                    />
-                                </div>
-                            }
-                        >
+                        <Table isStriped aria-label="table" className="h-full">
                             <TableHeader>
                                 <TableColumn align="center">{locale.RecordFieldColumn}</TableColumn>
                                 <TableColumn align="center">{locale.RecordValueColumn}</TableColumn>
+                                <TableColumn align="center">{locale.RecordActionColumn}</TableColumn>
                             </TableHeader>
                             <TableBody className="h-full" emptyContent={<div>{locale.EmptyRecordSelect}</div>}>
                                 {fieldList
-                                    .slice((fieldpage - 1) * 10, fieldpage * 10)
                                     .filter(() => itemChoose)
                                     .map(({ id: field_id, field_name, radios }) => {
                                         const record = recordList
@@ -176,11 +180,20 @@ const Component = () => {
                                             record?.field_value;
                                         return (
                                             <TableRow>
-                                                <TableCell className="min-w-32" align="center">
+                                                <TableCell className="min-w-48 max-w-48" align="center">
                                                     {field_name}
                                                 </TableCell>
                                                 <TableCell className="min-w-32" align="center">
                                                     {value}
+                                                </TableCell>
+                                                <TableCell className="min-w-24 max-w-24" align="center">
+                                                    <Button
+                                                        variant="bordered"
+                                                        size="sm"
+                                                        onClick={() => value && copytext(String(value))}
+                                                    >
+                                                        复制
+                                                    </Button>
                                                 </TableCell>
                                             </TableRow>
                                         );
